7b945c66986b73839395af910f1de9c35746a7bf
MELISSA <025110617K@utea.edu.pe>
2026-05-18 08:51:42 -0500

    for;while

---
+++ EjemploWhile.java
@@ -0,0 +1,10 @@
+public class EjemploWhile {
+    public static void main(String[] args) {
+        //while, para repetir intrucciones sin un final especifico
+        int i = 1;
+        while (i < 11) {
+            System.out.println(i); // del 1 al 10
+            i++;
+        }
+    }  
+}
